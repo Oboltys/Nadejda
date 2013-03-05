bbd76e6d00f64d55a5bf1a1f2663e7ebe93addae
Pavel <Oboltyss@Gmail.com>
2013-03-05 21:45:22 +0700

    V 1.0: + БД Nadejda лежит в SQL Azure. + Добавил два экрана для работы с данными.
    Bugs:
    - Проблема с индексными полями при добавлении данных

---
+++ Nadejda/Client/UserCode/CreateNewПерсональные_данныеItem.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using System.IO;
+using System.IO.IsolatedStorage;
+using System.Collections.Generic;
+using Microsoft.LightSwitch;
+using Microsoft.LightSwitch.Framework.Client;
+using Microsoft.LightSwitch.Presentation;
+using Microsoft.LightSwitch.Presentation.Extensions;
+
+namespace LightSwitchApplication
+{
+    public partial class CreateNewПерсональные_данныеItem
+    {
+        partial void CreateNewПерсональные_данныеItem_InitializeDataWorkspace(global::System.Collections.Generic.List<global::Microsoft.LightSwitch.IDataService> saveChangesTo)
+        {
+            // Write your code here.
+            this.Персональные_данныеItemProperty = new Персональные_данныеItem();
+        }
+
+        partial void CreateNewПерсональные_данныеItem_Saved()
+        {
+            // Write your code here.
+            this.Close(false);
+            Application.Current.ShowDefaultScreen(this.Персональные_данныеItemProperty);
+        }
+    }
+}
---
+++ Nadejda/Client/UserCode/Персональные_данныеItemDetail.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using System.IO;
+using System.IO.IsolatedStorage;
+using System.Collections.Generic;
+using Microsoft.LightSwitch;
+using Microsoft.LightSwitch.Framework.Client;
+using Microsoft.LightSwitch.Presentation;
+using Microsoft.LightSwitch.Presentation.Extensions;
+
+namespace LightSwitchApplication
+{
+    public partial class Персональные_данныеItemDetail
+    {
+        partial void Персональные_данныеItem_Loaded(bool succeeded)
+        {
+            // Write your code here.
+            this.SetDisplayNameFromEntity(this.Персональные_данныеItem);
+        }
+
+        partial void Персональные_данныеItem_Changed()
+        {
+            // Write your code here.
+            this.SetDisplayNameFromEntity(this.Персональные_данныеItem);
+        }
+
+        partial void Персональные_данныеItemDetail_Saved()
+        {
+            // Write your code here.
+            this.SetDisplayNameFromEntity(this.Персональные_данныеItem);
+        }
+    }
+}
